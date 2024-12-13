422baba07174511f298edd29cac0d8e9dc57db93
Giovanni Perez <giovanni.perez@outlook.com>
2024-12-13 12:17:35 -0600

    feat: add authentication

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,15 @@ use App\Http\Middleware\Metrics\LogResponseTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+/* Authentication */
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () 
+{
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
 Route::get('/feed', [FlickrController::class, 'getFeed'])->middleware([LogResponseTime::class]);
